4e95ce90f92f077253fcc8d75173922cf5c892c3
Drednine <kroxa201337@gmail.com>
2025-05-22 21:56:47 +0300

    fix: Обновлены зависимости и конфигурация Next.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['llkacrchwiefgdgmuixl.supabase.co'],
+    unoptimized: true
   },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_OZON_API_URL: process.env.NEXT_PUBLIC_OZON_API_URL,
   },
+  experimental: {
+    serverActions: true
+  }
 };
 
 module.exports = nextConfig;
